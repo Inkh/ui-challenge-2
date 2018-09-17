c843af2a86346b6d7875c4a60dc4903a482eb682
Jeff <jhwe92@gmail.com>
2018-09-17 14:40:46 -0700

    Error message now generates live

--- js/app.js
+++ js/app.js
@@ -2,9 +2,7 @@
 
 console.log('link');
 
-$('.sel').children().each((() => {
-  console.log('i child');
-}));
+
 
 //Radio listener
 $('.rad-group').on('click', function () {
@@ -21,12 +19,29 @@ $('.check-group').on('click', function () {
   }
 });
 
-$('.form').on('submit', function (e) {
-  e.preventDefault();
-  if ($('#input').val().length < 2) {
-    $('#input').addClass('error').after('<p class="text-err">please enter a valid username</p>');
+
+// $('.form').on('submit', function (e) {
+//   e.preventDefault();
+//   if ($('#input').val().length < 2) {
+//     $('#input').addClass('error').after('<p class="text-err">please enter a valid username</p>');
+//   }
+// })
+
+$('#input').on('input', function () {
+  if ($(this).val().length < 2) {
+    $('#input').addClass('error');
+    $('.text-err').show();
+  } else {
+    $('#input').removeClass('error');
+    $('.text-err').hide();
   }
 })
 
+$('.sel').append()
+$('select').children().each(() => {
+  console.log('i here');
+})
 
-
+$('.sel:nth-child(2)').children().each(() => {
+  console.log('i child');
+});
