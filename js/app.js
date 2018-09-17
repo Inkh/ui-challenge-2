a4484a640d1a92c206f211d03d4aa91d3958a951
Jeff <jhwe92@gmail.com>
2018-09-17 12:55:45 -0700

    Radio button working

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 'use strict';
 
 console.log('link');
+
+// $('.rad-group')
+// $('.radio').on('click', function (e) {
+// console.log($(this));
+// $(this).toggleClass('radio-select');
+// e.target.addClass('radio-select');
+// console.log($(this).siblings()[0].toggleClass('radio-select'))
+// })
+
+$('.sel')
+$('.sel').children().each((() => {
+  console.log('i child');
+}));
+
+$('.rad-group').on('click', function () {
+  console.log($(this));
+  console.log($(this).children()[0])
+  $(this).children().prop('checked', 'checked');
+  console.log($(this).children().val())
+})
