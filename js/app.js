e266efcd11fc810ae8dca6c35b35544cda7d0863
Jeff <jhwe92@gmail.com>
2018-09-17 13:15:59 -0700

    Added error message on failed validation

--- js/app.js
+++ js/app.js
@@ -6,11 +6,13 @@ $('.sel').children().each((() => {
   console.log('i child');
 }));
 
+//Radio listener
 $('.rad-group').on('click', function () {
   $(this).children().prop('checked', true);
   console.log($(this).children().val())
 });
 
+//Checkbox listener
 $('.check-group').on('click', function () {
   if ($(this).children().prop('checked')) {
     $(this).children().prop('checked', false);
@@ -18,6 +20,13 @@ $('.check-group').on('click', function () {
     $(this).children().prop('checked', true);
   }
 });
+
+$('.form').on('submit', function (e) {
+  e.preventDefault();
+  if ($('#input').val().length < 2) {
+    $('#input').addClass('error').after('<p class="text-err">please enter a valid username</p>');
+  }
+})
 
 
 
