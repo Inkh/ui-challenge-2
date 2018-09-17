b67a0ef0ba97e316aedb03658c1ff554723a9f8f
Jeff <jhwe92@gmail.com>
2018-09-17 13:06:48 -0700

    Checkbox working, slightly styled submit button

--- js/app.js
+++ js/app.js
@@ -2,22 +2,22 @@
 
 console.log('link');
 
-// $('.rad-group')
-// $('.radio').on('click', function (e) {
-// console.log($(this));
-// $(this).toggleClass('radio-select');
-// e.target.addClass('radio-select');
-// console.log($(this).siblings()[0].toggleClass('radio-select'))
-// })
-
-$('.sel')
 $('.sel').children().each((() => {
   console.log('i child');
 }));
 
 $('.rad-group').on('click', function () {
-  console.log($(this));
-  console.log($(this).children()[0])
-  $(this).children().prop('checked', 'checked');
+  $(this).children().prop('checked', true);
   console.log($(this).children().val())
-})
+});
+
+$('.check-group').on('click', function () {
+  if ($(this).children().prop('checked')) {
+    $(this).children().prop('checked', false);
+  } else {
+    $(this).children().prop('checked', true);
+  }
+});
+
+
+
